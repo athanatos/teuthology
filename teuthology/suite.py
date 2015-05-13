6da90ba393478ab0d02a8e5bd768e92f2bd986cd
Samuel Just <sjust@redhat.com>
2015-05-13 11:19:32 -0700

    working

--- teuthology/suite.py
+++ teuthology/suite.py
@@ -780,6 +780,7 @@ def build_matrix(path, _isfile=os.path.isfile,
 
 def _build_matrix(path, _isfile=os.path.isfile,
                   _isdir=os.path.isdir, _listdir=os.listdir, mincyclicity=0, item=''):
+    assert False
     if _isfile(path):
         if path.endswith('.yaml'):
             return matrix.Base(item)
